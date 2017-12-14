6b9ff9e1015516ff6cc28848d3ef7abc635aeb2d
Jason Gerard DeRose <jason@system76.com>
2017-12-14 15:32:32 -0700

    Add Store.sig_path() method

--- src/store.rs
+++ src/store.rs
@@ -14,6 +14,10 @@ impl Store {
         return self.basedir.join(hex::encode(key));
     }
 
+    pub fn sig_path(&self, sig: &[u8; 400]) -> PathBuf {
+        return self.path(&sig[0..64]);
+    }
+
 }
 
 
@@ -30,5 +34,15 @@ mod tests {
         assert_eq!(s.path(&[255]).as_path(), Path::new("/nope/ff"));
         assert_eq!(s.path(&[0, 255]).as_path(), Path::new("/nope/00ff"));
         assert_eq!(s.path(&[255, 0]).as_path(), Path::new("/nope/ff00"));
+    }
+
+    #[test]
+    fn test_sig_path() {
+        let s = Store::new("/nope");
+        let sig = [0u8; 400];
+        assert_eq!(
+            s.sig_path(&sig).as_path(),
+            Path::new("/nope/00000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000")
+        );
     }
 }
