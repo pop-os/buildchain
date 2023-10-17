cb68f2b94a802222256979d72f51a52b8520b216
Tim Crawford <tcrawford@system76.com>
2023-10-17 11:23:57 -0600

    Remove use of LXD
    Having buildchain only operate locally leaves where it actually runs as
    a higher-level decision for the project requirements and infrastructure.
    
    e.g.:
    
    - In a VM, using GitHub Actions or GitLab CI
    - In a Docker container, using Woodpecker
    - On a physical machine, using Jenkins
    
    Signed-off-by: Tim Crawford <tcrawford@system76.com>

--- src/config.rs
+++ src/config.rs
@@ -7,15 +7,10 @@ use serde::{Deserialize, Serialize};
 pub struct Config {
     /// The name of this build project
     pub name: String,
-    /// The LXC base to use
-    pub base: String,
-    /// True if the LXC container for builds should be privileged
-    #[serde(default = "Default::default")]
-    pub privileged: bool,
     /// The commands to run to generate a build environment
     pub prepare: Vec<Vec<String>>,
-    /// The commands to run that build the artifacts in /root/source
+    /// The commands to run that build the artifacts in `source/`
     pub build: Vec<Vec<String>>,
-    /// The commands to run that publish the artifacts to /root/artifacts
+    /// The commands to run that publish the artifacts to `artifacts/`
     pub publish: Vec<Vec<String>>,
 }
